020c125dca1dcd7d537d7cdd4dc0bb80e708632f
ninjadragon008@icloud.com <u7447182@anu.edu.au>
2022-03-23 15:13:35 +1100

    still thinking through the setup; updated to 1.18

---
+++ src/main/java/com/amuzil/omegasource/skillapi/activators/KeyCombination.java
@@ -0,0 +1,14 @@
+package com.amuzil.omegasource.skillapi.activators;
+
+import com.mojang.blaze3d.platform.InputConstants;
+
+import java.util.ArrayList;
+
+/**
+ * Essentially a wrapper class for a part of combination/sequential list of keys.
+ */
+public class KeyCombination {
+
+    private ArrayList<InputConstants.Key> keys;
+
+}
---
+++ src/main/java/com/amuzil/omegasource/skillapi/activators/KeyCombo.java
@@ -0,0 +1,7 @@
+package com.amuzil.omegasource.skillapi.activators;
+
+/**
+ * Wrapper class for the whole sequential list of keys.
+ */
+public class KeyCombo {
+}
