3b332341aae276bf57f7fb8caed80610baf171aa
ninjadragon008@icloud.com <u7447182@anu.edu.au>
2022-12-24 15:04:45 -0800

    Documentation for IData

--- src/main/java/com/amuzil/omegasource/magus/skill/util/capability/IData.java
+++ src/main/java/com/amuzil/omegasource/magus/skill/util/capability/IData.java
@@ -4,6 +4,10 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.common.capabilities.AutoRegisterCapability;
 import net.minecraftforge.common.util.INBTSerializable;
 
+/**
+ * Very different from IDataTrait. This class lets you access data and do things with it,
+ * whereas IDataTrait merely stores it.
+ */
 @AutoRegisterCapability
 public interface IData extends INBTSerializable<CompoundTag> {
 
