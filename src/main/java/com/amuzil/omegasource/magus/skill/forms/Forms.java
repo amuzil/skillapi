5ae5ecde95f058b1a5833a0881148f2e9d19e99d
aidan99davis <aidanpauldavis@yahoo.co.uk>
2023-02-02 23:26:31 +0000

    fixed compilation error

--- src/main/java/com/amuzil/omegasource/magus/skill/forms/Forms.java
+++ src/main/java/com/amuzil/omegasource/magus/skill/forms/Forms.java
@@ -1,18 +1,20 @@
 package com.amuzil.omegasource.magus.skill.forms;
 
+import java.util.LinkedList;
+
 public class Forms {
     // FORMS
-    public static final Form PUSH = new Form("push");
-    public static final Form PULL = new Form("pull");
-    public static final Form RAISE = new Form("raise");
-    public static final Form LOWER = new Form("lower");
-    public static final Form BURST = new Form("burst");
-    public static final Form ARC = new Form("arc");
-    public static final Form COMPRESS = new Form("compress");
-    public static final Form EXPAND = new Form("expand");
-    public static final Form TWIST = new Form("twist");
-    public static final Form STRIKE = new Form("strike");
-    public static final Form BLOCK = new Form("block");
-    public static final Form BREATHE = new Form("breathe");
-    public static final Form STEP = new Form("step");
+    public static final Form PUSH = new Form("push", new LinkedList<>());
+    public static final Form PULL = new Form("pull", new LinkedList<>());
+    public static final Form RAISE = new Form("raise", new LinkedList<>());
+    public static final Form LOWER = new Form("lower", new LinkedList<>());
+    public static final Form BURST = new Form("burst", new LinkedList<>());
+    public static final Form ARC = new Form("arc", new LinkedList<>());
+    public static final Form COMPRESS = new Form("compress", new LinkedList<>());
+    public static final Form EXPAND = new Form("expand", new LinkedList<>());
+    public static final Form TWIST = new Form("twist", new LinkedList<>());
+    public static final Form STRIKE = new Form("strike", new LinkedList<>());
+    public static final Form BLOCK = new Form("block", new LinkedList<>());
+    public static final Form BREATHE = new Form("breathe", new LinkedList<>());
+    public static final Form STEP = new Form("step", new LinkedList<>());
 }
