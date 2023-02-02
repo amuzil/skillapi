fc7216433cc313146820c9e355c23a3295c9f233
aidan99davis <aidanpauldavis@yahoo.co.uk>
2023-02-02 23:23:58 +0000

    remove artifacts of an old implementation attempt

--- src/main/java/com/amuzil/omegasource/magus/input/InputModule.java
+++ src/main/java/com/amuzil/omegasource/magus/input/InputModule.java
@@ -1,17 +1,15 @@
 package com.amuzil.omegasource.magus.input;
 
 import com.amuzil.omegasource.magus.radix.Condition;
-import com.amuzil.omegasource.magus.skill.conditionals.InputData;
 import com.amuzil.omegasource.magus.skill.forms.Form;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public abstract class InputModule {
     protected final Map<Condition, Form> _formInputs = new HashMap<>();
 
-    public abstract Condition registerInputData(Condition formCondition, Form formToExecute, Runnable onSuccess, Runnable onFailure);
+    public abstract void registerInputData(Condition formCondition, Form formToExecute, Runnable onSuccess, Runnable onFailure);
 
     public void unregisterCondition(Condition condition) {
         condition.unregister();
--- src/main/java/com/amuzil/omegasource/magus/input/KeyboardMouseInputModule.java
+++ src/main/java/com/amuzil/omegasource/magus/input/KeyboardMouseInputModule.java
@@ -1,27 +1,19 @@
 package com.amuzil.omegasource.magus.input;
 
 import com.amuzil.omegasource.magus.radix.Condition;
-import com.amuzil.omegasource.magus.radix.RadixTree;
-import com.amuzil.omegasource.magus.skill.conditionals.ConditionBuilder;
-import com.amuzil.omegasource.magus.skill.conditionals.InputData;
 import com.amuzil.omegasource.magus.skill.forms.Form;
-
-import java.util.List;
 
 public class KeyboardMouseInputModule extends InputModule {
 
     @Override
-    public Condition registerInputData(Condition formCondition, Form formToExecute, Runnable onSuccess, Runnable onFailure) {
+    public void registerInputData(Condition formCondition, Form formToExecute, Runnable onSuccess, Runnable onFailure) {
         if(formCondition != null) {
             //Register listeners for condition created.
             formCondition.register(onSuccess, onFailure);
             //add condition to InputModule registry so that it can be tracked.
             _formInputs.put(formCondition, formToExecute);
-
-            return formCondition;
         } else {
             //todo errors/logging
-            return null;
         }
     }
 }
