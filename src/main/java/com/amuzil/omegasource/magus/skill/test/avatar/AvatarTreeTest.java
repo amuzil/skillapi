d13d776fc66e1930d2d792e58c7e53e571680ac2
FavouriteDragon <ninjadragon008@icloud.com>
2023-08-29 00:14:27 +1000

    test method

--- src/main/java/com/amuzil/omegasource/magus/skill/test/avatar/AvatarTreeTest.java
+++ src/main/java/com/amuzil/omegasource/magus/skill/test/avatar/AvatarTreeTest.java
@@ -4,4 +4,9 @@ package com.amuzil.omegasource.magus.skill.test.avatar;
  * Tests out techniques using the radix tree.
  */
 public class AvatarTreeTest {
+
+    public static void registerTechniques() {
+
+        //Air Gust: Arc > Strike
+    }
 }
