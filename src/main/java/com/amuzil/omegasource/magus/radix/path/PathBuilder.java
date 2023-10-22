823e715a417d9a77cdd57276c3d3b5da55c651c9
FavouriteDragon <ninjadragon008@icloud.com>
2023-10-22 16:28:09 +1100

    Added in a placeholder for the mouse wheel condition

--- src/main/java/com/amuzil/omegasource/magus/radix/path/PathBuilder.java
+++ src/main/java/com/amuzil/omegasource/magus/radix/path/PathBuilder.java
@@ -9,7 +9,6 @@ import com.amuzil.omegasource.magus.radix.condition.minecraft.forge.key.KeyPress
 import com.amuzil.omegasource.magus.skill.conditionals.key.KeyCombination;
 import com.amuzil.omegasource.magus.skill.conditionals.key.KeyInput;
 import com.amuzil.omegasource.magus.skill.conditionals.key.KeyPermutation;
-import com.amuzil.omegasource.magus.skill.conditionals.mouse.MouseInput;
 import com.amuzil.omegasource.magus.skill.conditionals.mouse.MouseWheelInput;
 import net.minecraftforge.event.TickEvent;
 
@@ -19,62 +18,71 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class PathBuilder {
-	private static final Map<Class<?>, Function<Object, LinkedList<Condition>>> CONDITION_BUILDERS = new HashMap<>();
+    private static final Map<Class<?>, Function<Object, LinkedList<Condition>>> CONDITION_BUILDERS = new HashMap<>();
 
-	static {
-		/* Keys. */
-		//TODO: Account for max delay
-		registerBuilder(KeyInput.class, keyInput -> {
-			// Minimum amount of ticks a key must be pressed for it to be considered a held condition.
-			//TODO: Adjust these
-			final int HELD_THRESHOLD = 3;
-			// 50 by default
-			final int TIMEOUT_THRESHOLD = 50000;
+    static {
+        /* Keys. */
+        //TODO: Account for max delay
+        registerBuilder(KeyInput.class, keyInput -> {
+            // Minimum amount of ticks a key must be pressed for it to be considered a held condition.
+            //TODO: Adjust these
+            final int HELD_THRESHOLD = 3;
+            // 50 by default
+            final int TIMEOUT_THRESHOLD = 6000;
 
-			LinkedList<Condition> conditions = new LinkedList<>();
+            LinkedList<Condition> conditions = new LinkedList<>();
 
-			// Any time less than this is just a key press.
-			// TODO: Adjust timeout to be per node.
-			conditions.add(keyInput.held() > HELD_THRESHOLD
-				? new KeyHoldCondition(keyInput.key().getValue(), keyInput.held(), TIMEOUT_THRESHOLD)
-				: new KeyPressCondition(keyInput.key().getValue(), TIMEOUT_THRESHOLD)
-			);
+            // Any time less than this is just a key press.
+            // TODO: Adjust timeout to be per node.
+            conditions.add(keyInput.held() > HELD_THRESHOLD
+                    ? new KeyHoldCondition(keyInput.key().getValue(), keyInput.held(), TIMEOUT_THRESHOLD)
+                    : new KeyPressCondition(keyInput.key().getValue(), TIMEOUT_THRESHOLD)
+            );
 
-			if (keyInput.minDelay() > 0) {
-				//TODO: Fix this to account for "action keys".
-				conditions.add(new TickTimedCondition(
-					TickEvent.Type.CLIENT, TickEvent.Phase.START,
-					keyInput.maxDelay(), Result.SUCCESS,
-					new KeyPressedCondition(TIMEOUT_THRESHOLD), Result.FAILURE, Result.SUCCESS
-				));
-			}
+            if (keyInput.minDelay() > 0) {
+                //TODO: Fix this to account for "action keys".
+                conditions.add(new TickTimedCondition(
+                        TickEvent.Type.CLIENT, TickEvent.Phase.START,
+                        keyInput.maxDelay(), Result.SUCCESS,
+                        new KeyPressedCondition(TIMEOUT_THRESHOLD), Result.FAILURE, Result.SUCCESS
+                ));
+            }
 
-			return conditions;
-		});
-		registerBuilder(KeyPermutation.class,
-			permutation -> permutation.keys().stream().map(PathBuilder::buildPathFrom)
-				.collect(LinkedList::new, LinkedList::addAll, LinkedList::addAll)
-		);
-		registerBuilder(KeyCombination.class,
-			combination -> combination.keys().stream().map(PathBuilder::buildPathFrom)
-				.collect(LinkedList::new, LinkedList::addAll, LinkedList::addAll)
-		);
+            return conditions;
+        });
+        registerBuilder(KeyPermutation.class,
+                permutation -> permutation.keys().stream().map(PathBuilder::buildPathFrom)
+                        .collect(LinkedList::new, LinkedList::addAll, LinkedList::addAll)
+        );
+        registerBuilder(KeyCombination.class,
+                combination -> combination.keys().stream().map(PathBuilder::buildPathFrom)
+                        .collect(LinkedList::new, LinkedList::addAll, LinkedList::addAll)
+        );
 
-		/* Mouse */
+        /* Mouse */
 //		registerBuilder(MouseInput.class,
 //				mouseInput -> );
-//		registerBuilder(MouseWheelInput.class,
-//				mouseWheelInput -> );
-	}
+        registerBuilder(MouseWheelInput.class,
+                mouseWheelInput -> {
+					final int TIMEOUT_THRESHOLD = 6000;
+                    LinkedList<Condition> conditions = new LinkedList<>();
+                    // Placeholder for now
+					conditions.add(new KeyPressCondition(0, TIMEOUT_THRESHOLD));
 
-	@SuppressWarnings("unchecked")
-	public static <T> void registerBuilder(Class<T> type, Function<T, LinkedList<Condition>> builder) {
-		CONDITION_BUILDERS.put(type, (Function<Object, LinkedList<Condition>>) builder);
-	}
+					return conditions;
 
-	// TODO turn a linked list of conditions into a connected node tree.
+                }
+        );
+    }
 
-	public static <T> LinkedList<Condition> buildPathFrom(T data) {
-		return CONDITION_BUILDERS.get(data.getClass()).apply(data);
-	}
+    @SuppressWarnings("unchecked")
+    public static <T> void registerBuilder(Class<T> type, Function<T, LinkedList<Condition>> builder) {
+        CONDITION_BUILDERS.put(type, (Function<Object, LinkedList<Condition>>) builder);
+    }
+
+    // TODO turn a linked list of conditions into a connected node tree.
+
+    public static <T> LinkedList<Condition> buildPathFrom(T data) {
+        return CONDITION_BUILDERS.get(data.getClass()).apply(data);
+    }
 }
