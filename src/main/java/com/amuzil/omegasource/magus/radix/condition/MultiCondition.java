55d9dc4c17728f6f42778be5c2032b870ab01095
Aidan <aidanpauldavis1999@gmail.com>
2024-02-02 23:34:23 +0000

    changed multi condition

--- src/main/java/com/amuzil/omegasource/magus/radix/condition/MultiCondition.java
+++ src/main/java/com/amuzil/omegasource/magus/radix/condition/MultiCondition.java
@@ -1,41 +1,51 @@
 package com.amuzil.omegasource.magus.radix.condition;
 
 import com.amuzil.omegasource.magus.radix.Condition;
-import org.apache.logging.log4j.LogManager;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiCondition extends Condition {
     private final List<Condition> concurrentConditions;
     private Runnable onCompleteSuccess;
     private Runnable onCompleteFailure;
-    private final Runnable onPartialSuccess;
-    private int conditionsMet;
+    private Dictionary<Integer, Boolean> conditionsMet;
 
     public MultiCondition(List<Condition> concurrentConditions) {
         this.concurrentConditions = concurrentConditions;
-        this.onPartialSuccess = this::conditionMet;
     }
 
     public MultiCondition(Condition condition) {
         this.concurrentConditions = List.of(condition);
-        this.onPartialSuccess = this::conditionMet;
     }
 
-    private void conditionMet() {
-        this.conditionsMet++;
-
-        if(this.conditionsMet == concurrentConditions.size()) {
-            this.onCompleteSuccess.run();
-            this.conditionsMet = 0;
+    private void checkConditionMet() {
+        for (Iterator<Boolean> it = conditionsMet.elements().asIterator(); it.hasNext(); ) {
+            // none/not all conditions have been met yet, exit loop and dont execute.
+            if (it.next() == false) return;
         }
+        this.onCompleteSuccess.run();
+        this.reset();
     }
 
     @Override
     public void register(Runnable onSuccess, Runnable onFailure) {
         this.onCompleteSuccess = onSuccess;
         this.onCompleteFailure = onFailure;
-        concurrentConditions.forEach(condition -> condition.register(onPartialSuccess, onCompleteFailure));
+        this.reset();
+    }
+
+    public void reset() {
+        AtomicInteger counter = new AtomicInteger();
+        conditionsMet = new Hashtable<>();
+        concurrentConditions.forEach(condition -> {
+            int id = counter.getAndIncrement();
+            condition.register(() -> {
+                conditionsMet.put(id, true);
+                checkConditionMet();
+            }, onCompleteFailure);
+            conditionsMet.put(id, false);
+        });
     }
 
     @Override
@@ -43,6 +53,3 @@ public class MultiCondition extends Condition {
         concurrentConditions.forEach(Condition::unregister);
     }
 }
-
-
-
