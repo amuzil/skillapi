68ec47e72cc953cd65e43a24d15fa39bf9322409
Aidan <aidanpauldavis1999@gmail.com>
2024-02-06 13:24:44 +0000

    changes

--- src/main/java/com/amuzil/omegasource/magus/radix/condition/MultiCondition.java
+++ src/main/java/com/amuzil/omegasource/magus/radix/condition/MultiCondition.java
@@ -1,6 +1,9 @@
 package com.amuzil.omegasource.magus.radix.condition;
 
+import com.amuzil.omegasource.magus.Magus;
+import com.amuzil.omegasource.magus.input.KeyboardMouseInputModule;
 import com.amuzil.omegasource.magus.radix.Condition;
+import net.minecraftforge.event.TickEvent;
 import org.apache.logging.log4j.LogManager;
 
 import java.util.Dictionary;
@@ -8,12 +11,17 @@ import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 
 public class MultiCondition extends Condition {
     private final List<Condition> concurrentConditions;
     private Runnable onCompleteSuccess;
     private Runnable onCompleteFailure;
     private Dictionary<Integer, Boolean> conditionsMet;
+    private Consumer<TickEvent.ClientTickEvent> clientTickListener;
+    private static int TIMEOUT_IN_TICKS = 15;
+    private int executionTime = 0;
+    private boolean startedExecuting = false;
 
     public MultiCondition(List<Condition> concurrentConditions) {
         this.concurrentConditions = concurrentConditions;
@@ -34,6 +42,20 @@ public class MultiCondition extends Condition {
 
     @Override
     public void register(Runnable onSuccess, Runnable onFailure) {
+        this.clientTickListener = event -> {
+            if (event.phase == TickEvent.ClientTickEvent.Phase.START) {
+                if(startedExecuting)
+                {
+                    executionTime++;
+                    if(executionTime > TIMEOUT_IN_TICKS) {
+                        this.onCompleteFailure.run();
+
+                        LogManager.getLogger().info("MULTI CONDITION TIMED OUT");
+                        this.reset();
+                    }
+                }
+            }
+        };
         this.onCompleteSuccess = onSuccess;
         this.onCompleteFailure = onFailure;
         this.reset();
@@ -42,18 +64,26 @@ public class MultiCondition extends Condition {
     public void reset() {
         AtomicInteger counter = new AtomicInteger();
         conditionsMet = new Hashtable<>();
+        this.startedExecuting = false;
+        this.executionTime = 0;
         concurrentConditions.forEach(condition -> {
             int id = counter.getAndIncrement();
             condition.register(() -> {
                 synchronized (conditionsMet) {
                     // Debugging statement:
 //                    LogManager.getLogger().info("MARKING CONDITION MET: " + concurrentConditions.get(id).getClass());
-
+                    startedExecuting = true;
                     conditionsMet.put(id, true);
                     condition.unregister();
                 }
                 checkConditionMet();
-            }, onCompleteFailure);
+            }, () -> {
+                synchronized (conditionsMet) {
+                    conditionsMet.put(id, false);
+                    condition.unregister();
+                }
+                checkConditionMet();
+            });
             conditionsMet.put(id, false);
         });
     }
