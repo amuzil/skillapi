c13b688934f3b5ff5c343ca70146862e0b9e2780
FavouriteDragon <ninjadragon008@icloud.com>
2024-10-26 12:45:03 +1100

    Slowly adding in special logic so that avatar is get up and go

--- src/main/java/com/amuzil/omegasource/magus/skill/util/capability/entity/Magi.java
+++ src/main/java/com/amuzil/omegasource/magus/skill/util/capability/entity/Magi.java
@@ -3,9 +3,11 @@ package com.amuzil.omegasource.magus.skill.util.capability.entity;
 import com.amuzil.omegasource.magus.radix.RadixTree;
 import com.amuzil.omegasource.magus.skill.skill.Skill;
 import com.amuzil.omegasource.magus.skill.util.capability.CapabilityHandler;
+import com.amuzil.omegasource.magus.skill.util.data.SkillCategoryData;
 import com.amuzil.omegasource.magus.skill.util.data.SkillData;
 import com.amuzil.omegasource.magus.skill.util.traits.DataTrait;
 import com.amuzil.omegasource.magus.skill.util.traits.SkillTrait;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -13,11 +15,14 @@ import net.minecraftforge.common.util.FakePlayer;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Magi {
 
 
     private final Data capabilityData;
+    private List<SkillData> skillData;
+    private List<SkillCategoryData> skillCategoryData;
 
     public Magi(Data capabilityData) {
         this.capabilityData = capabilityData;
@@ -43,14 +48,35 @@ public class Magi {
         // Get skill data based on the skill,
         // then get the list of its traits.
         // Empty for now...
-        return new LinkedList<>();
+        return getSkillData(skill).getSkillTraits();
     }
 
     public static boolean isEntitySupported(Entity entity) {
         return entity instanceof LivingEntity;
     }
 
+    // Returns appropriate SkillData from a list of SkillData
     public SkillData getSkillData(Skill skill) {
-        return null;
+        return getSkillData(skill.getId());
     }
+
+    public SkillData getSkillData(ResourceLocation id) {
+        return skillData.stream().filter(skillData1 -> skillData1.getSkillId().equals(id)).toList().get(0);
+    }
+
+    public SkillData getSkillData(String id) {
+        ResourceLocation loc = new ResourceLocation(id);
+        return getSkillData(loc);
+    }
+
+    // Called per tick
+    public void onUpdate() {
+
+    }
+
+    public void onDeath() {
+
+    }
+
+
 }
---
+++ src/main/java/com/amuzil/omegasource/magus/skill/util/traits/entitytraits/EnergyTrait.java
@@ -0,0 +1,61 @@
+package com.amuzil.omegasource.magus.skill.util.traits.entitytraits;
+
+import com.amuzil.omegasource.magus.skill.util.traits.DataTrait;
+import net.minecraft.nbt.CompoundTag;
+
+/**
+ * This serves as a generic wrapper for any kind of meter/energy bar for the player.
+ * Chi, mana, qi, nen, stamina, whatever. Other such classes should extend this, because this is the class that will be
+ * ticked and adjusted in Magi.java
+ *
+ * Example of a custom implementation: Chi.java for the Avatar Mod rework.
+ * Instead of just the values contained in this class, it will also track your "available" amount of energy, and adjust
+ * based on that.
+ *
+ * Of course, if you need more specific custom behaviour in terms of when to regenerate, how to regenerate energy, e.t.c,
+ * then make a child class extending Magi and add your logic there.
+ */
+public class EnergyTrait implements DataTrait {
+    private String name;
+    private double maxEnergy;
+    private double currentEnergy;
+    private boolean isDirty = false;
+
+    public EnergyTrait(String name, double maxEnergy) {
+        this.name = name;
+        this.maxEnergy = maxEnergy;
+        this.currentEnergy = maxEnergy;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void markDirty() {
+        this.isDirty = true;
+    }
+
+    @Override
+    public void markClean() {
+        this.isDirty = false;
+    }
+
+    @Override
+    public boolean isDirty() {
+        return this.isDirty;
+    }
+
+    @Override
+    public CompoundTag serializeNBT() {
+        return null;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundTag nbt) {
+        this.name = nbt.getString("name");
+        this.maxEnergy = nbt.getDouble("max_energy");
+        this.currentEnergy = nbt.getDouble("current_energy");
+    }
+}
